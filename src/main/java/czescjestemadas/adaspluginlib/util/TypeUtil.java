c27e0d392dc5d8a7eee087643c00490d383549db
adas <adambimek@gmail.com>
2024-10-24 17:16:22 +0200

    null safe getter util

--- src/main/java/czescjestemadas/adaspluginlib/util/TypeUtil.java
+++ src/main/java/czescjestemadas/adaspluginlib/util/TypeUtil.java
@@ -3,6 +3,7 @@ package czescjestemadas.adaspluginlib.util;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.function.Function;
 
 public abstract class TypeUtil
 {
@@ -14,5 +15,15 @@ public abstract class TypeUtil
 	public static Type getParamType(Field field, int idx)
 	{
 		return ((ParameterizedType)field.getGenericType()).getActualTypeArguments()[idx];
+	}
+
+	public static <T, R> R safeGet(T obj, Function<T, R> getter)
+	{
+		return safeGet(obj, getter, null);
+	}
+
+	public static <T, R> R safeGet(T obj, Function<T, R> getter, R dflt)
+	{
+		return obj == null ? dflt : getter.apply(obj);
 	}
 }
